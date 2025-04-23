83d25cadf5b1920e678b63981c46c979df243b74
Alex Gil <alexgpareja@icloud.com>
2025-04-23 19:47:15 +0200

    Activitat 3 - Corretgit metode getNom segons indicacions del profe

--- 02-Xat_a_dos/src/ServidorXat.java
+++ 02-Xat_a_dos/src/ServidorXat.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 
@@ -34,10 +34,10 @@ public class ServidorXat {
         }
     }
 
-    public String getNom(InputStream input) {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(input))) {
-            return reader.readLine();
-        } catch (IOException e) {
+    public Object getNom(ObjectInputStream input) {
+        try {
+            return input.readObject();
+        } catch (IOException | ClassNotFoundException e) {
             System.err.println("Error al llegir el nom del client: " + e.getMessage());
             return null;
         }

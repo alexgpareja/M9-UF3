7cff6428726918ef601040872097b32f1d04d1e0
Alex Gil <alexgpareja@icloud.com>
2025-04-30 19:47:45 +0200

    Activitat 3 - Fitxer implementat

--- 03-Transferencia/src/Fitxer.java
+++ 03-Transferencia/src/Fitxer.java
@@ -1,3 +1,31 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Fitxer {
-    
+
+    private String nom;
+    private byte[] contingut;
+
+    public Fitxer(String nom) {
+        this.nom = nom;
+    }
+
+    public byte[] getContingut() throws IOException {
+
+        File file = new File(nom);
+        // Comprobar si el fitxer existeix
+        if (!file.exists()) {
+            throw new IOException("El fitxer no existeix: " + nom);
+        }
+
+        // Torna carregat el contingut del fitxer en bytes
+        try (FileInputStream fis = new FileInputStream(file)) {
+            int length = (int) file.length();
+            byte[] bytes = new byte[length];
+            fis.read(bytes);
+            return bytes;
+        }
+    }
 }
+    

c081642fc3fd93f37bd2f888033799a3c0c635e6
Alex Gil <alexgpareja@icloud.com>
2025-04-30 19:41:44 +0200

    Activitat 3 - Servidor metodes bàsics implementats

--- 03-Transferencia/src/Servidor.java
+++ 03-Transferencia/src/Servidor.java
@@ -1,3 +1,28 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
-    
+
+    // Constants
+    private static final int PORT = 9999;
+    private static final String HOST = "localhost";
+    ServerSocket serverSocket;
+
+    public Socket connectar() throws IOException {
+        serverSocket = new ServerSocket(PORT);
+        System.out.println("Acceptant connexions en -> " + HOST + ":" + PORT);
+        System.out.println("Esperant connexio...");
+        Socket socket = serverSocket.accept(); // Aceptar una conexión entrante
+        System.out.println("Connexio acceptada: " + socket.getInetAddress());
+        return socket;
+    }
+
+    public void tancarConnexio(Socket socket) throws IOException {
+        if (socket != null && !socket.isClosed()) {
+            socket.close();
+            System.out.println("Connexió tancada.");
+        }
+    }
+
 }
